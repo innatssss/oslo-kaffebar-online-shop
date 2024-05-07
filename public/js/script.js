ea0efcf8730aacc2944355139227c53b8adfe9a4
“inna_tssss” <inna.tspin11@gmail.com>
2024-05-07 16:59:14 +0200

    map

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+let marker;
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 52.531677, lng: 13.381777 },
+  });
+
+  marker = new google.maps.Marker({
+    map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 52.531677, lng: 13.381777 },
+  });
+  marker.addListener("click", toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+window.initMap = initMap;
